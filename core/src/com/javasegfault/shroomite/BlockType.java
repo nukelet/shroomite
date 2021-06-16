d1481c665061385a9513052521ea63cb19c47ccb
Gabriel Araujo <glucio314@gmail.com>
2021-06-16 13:01:03 -0300

    Add texture name to BlockType enum

--- core/src/com/javasegfault/shroomite/BlockType.java
+++ core/src/com/javasegfault/shroomite/BlockType.java
@@ -1,10 +1,20 @@
 package com.javasegfault.shroomite;
 
 public enum BlockType {
-	DIRT,
-	ROCK,
-	SAND,
-	WOOD,
-	WATER,
-	LAVA,
+	DIRT(TextureName.DIRT),
+	ROCK(TextureName.ROCK),
+	SAND(TextureName.SAND),
+	WOOD(TextureName.WOOD),
+	WATER(TextureName.WATER),
+	LAVA(TextureName.LAVA);
+	
+	private TextureName textureName;
+	
+	BlockType(TextureName textureName) {
+		this.textureName = textureName;
+	}
+	
+	TextureName getTextureName() {
+		return textureName;
+	}
 }
