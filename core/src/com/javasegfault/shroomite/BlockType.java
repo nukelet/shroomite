dec168b68254556494f83e96939f727151a284fa
Gabriel Araujo <glucio314@gmail.com>
2021-06-20 10:50:57 -0300

    Add value field in BlockType enum for serialization

--- core/src/com/javasegfault/shroomite/BlockType.java
+++ core/src/com/javasegfault/shroomite/BlockType.java
@@ -1,20 +1,39 @@
 package com.javasegfault.shroomite;
 
 public enum BlockType {
-    DIRT(TextureName.DIRT),
-    ROCK(TextureName.ROCK),
-    SAND(TextureName.SAND),
-    WOOD(TextureName.WOOD),
-    WATER(TextureName.WATER),
-    LAVA(TextureName.LAVA);
+    DIRT(TextureName.DIRT, "DIRT"),
+    ROCK(TextureName.ROCK, "ROCK"),
+    SAND(TextureName.SAND, "SAND"),
+    WOOD(TextureName.WOOD, "WOOD"),
+    WATER(TextureName.WATER, "WATER"),
+    LAVA(TextureName.LAVA, "LAVA"),
+    AIR(TextureName.SKY_4, "AIR");
 
     private TextureName textureName;
+    private String value;
 
-    BlockType(TextureName textureName) {
+    BlockType(TextureName textureName, String value) {
         this.textureName = textureName;
+        this.value = value;
     }
 
     public TextureName getTextureName() {
         return textureName;
+    }
+
+    public String toValue() {
+        return value;
+    }
+
+    public static BlockType fromValue(String value) {
+        if (value != null) {
+            for (BlockType blockType : BlockType.values()) {
+                if (blockType.toValue().equals(value)) {
+                    return blockType;
+                }
+            }
+        }
+
+        return null;
     }
 }
