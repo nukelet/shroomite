b61bb71defc27335d118aa13731eb4d0bee1ecd2
Gabriel Araujo <glucio314@gmail.com>
2021-06-26 19:13:59 -0300

    Refactor World

--- core/src/com/javasegfault/shroomite/World.java
+++ core/src/com/javasegfault/shroomite/World.java
@@ -1,8 +1,6 @@
 package com.javasegfault.shroomite;
 
 import com.javasegfault.shroomite.blocks.Block;
-import com.javasegfault.shroomite.blocks.RockBlock;
-import com.javasegfault.shroomite.blocks.SandBlock;
 import com.javasegfault.shroomite.util.Position;
 
 public class World {
@@ -16,22 +14,6 @@ public class World {
         this.blocks = new Block[width][height];
     }
 
-    public World() {
-        this(20, 20);
-
-        for (int i = 0; i < 5; i++) {
-            Position pos = new Position(10, 19 - i);
-            SandBlock sandBlock = new SandBlock(pos, this);
-            addBlock(sandBlock);
-        }
-
-        for (int i = 0; i < width; i++) {
-            Position pos = new Position(i, 0);
-            RockBlock rockBlock = new RockBlock(pos, this);
-            addBlock(rockBlock);
-        }
-    }
-
     public int getWidth() {
         return this.width;
     }
@@ -40,42 +22,91 @@ public class World {
         return this.height;
     }
 
-    // make sure that no blocks are overwritten?
+    /**
+     * Adds block to world at the given position.
+     * 
+     * @param position
+     * @param block
+     * @return True if position is valid, false otherwise.
+     */
+    public boolean setBlockAt(Position position, Block block) {
+        if (!isValidPosition(position)) {
+            return false;
+        }
+
+        setBlockAtNoCheck(position, block);
+        return true;
+    }
+
     public void addBlock(Block block) {
         Position pos = block.getPosition();
-        if (isValidPosition(pos)) {
-            if (getBlockAt(pos) != null) {
-                System.err.printf("Invalid block add at %s: position not empty\n", pos);
-                return;
-            } else {
-                blocks[pos.getX()][pos.getY()] = block;
-            }
-        } else {
+        if (!isValidPosition(pos)) {
             System.err.printf("Invalid block add at %s: invalid position\n", pos);
+            return;
         }
+
+        if (hasBlockAtNoCheck(pos)) {
+            System.err.printf("Invalid block add at %s: position not empty\n", pos);
+            return;
+        }
+
+        blocks[pos.getX()][pos.getY()] = block;
     }
 
-    private boolean isValidPosition(int x, int y) {
-        return x >= 0 && y >= 0 && x < width && y < height;
+    /**
+     * Adds block to world at the given position if the position is empty.
+     * 
+     * @param position
+     * @param block
+     * @return True if position is valid and there is no block at given position,
+     *         false otherwise.
+     */
+    public boolean addBlockAt(Position position, Block block) {
+        if (!isValidPosition(position)) {
+            return false;
+        }
+
+        if (hasBlockAtNoCheck(position)) {
+            return false;
+        }
+
+        setBlockAtNoCheck(position, block);
+        return true;
     }
 
-    private boolean isValidPosition(Position position) {
-        return isValidPosition(position.getX(), position.getY());
-    }
-
+    @Deprecated
     public Block getBlockAt(int x, int y) {
         if (!isValidPosition(x, y)) {
-            // System.err.printf("Invalid access to position Position(x=%d, y=%d): out of bounds\n", x, y);
+            // System.err.printf("Invalid access to position Position(x=%d, y=%d): out of
+            // bounds\n", x, y);
             return null;
         } else {
             return blocks[x][y];
         }
     }
 
+    /**
+     * @param position
+     * @return Block at given position or null if position is not valid.
+     */
     public Block getBlockAt(Position position) {
-        return getBlockAt(position.getX(), position.getY());
+        if (!isValidPosition(position)) {
+            return null;
+        }
+
+        return getBlockAtNoCheck(position);
     }
 
+    public boolean hasBlockAt(Position position) {
+        if (!isValidPosition(position)) {
+            System.err.printf("Invalid position: %s\n", position);
+            return false;
+        }
+
+        return hasBlockAtNoCheck(position);
+    }
+
+    @Deprecated
     public void removeBlock(Block block) {
         Position pos = block.getPosition();
         if (getBlockAt(pos) == null) {
@@ -83,5 +114,47 @@ public class World {
         } else {
             blocks[pos.getX()][pos.getY()] = null;
         }
+    }
+
+    /**
+     * Removes block from world at the given position.
+     * 
+     * @param position
+     * @return True if given position is valid, false otherwise.
+     */
+    public boolean removeBlockAt(Position position) {
+        if (!isValidPosition(position)) {
+            return false;
+        }
+
+        removeBlockAtNoCheck(position);
+        return true;
+    }
+
+    @Deprecated
+    private boolean isValidPosition(int x, int y) {
+        return x >= 0 && y >= 0 && x < width && y < height;
+    }
+
+    private boolean isValidPosition(Position position) {
+        return position.getX() >= 0 && position.getY() >= 0 && position.getX() < width
+                && position.getY() < height;
+    }
+
+    private Block getBlockAtNoCheck(Position position) {
+        return blocks[position.getX()][position.getY()];
+    }
+
+    private void setBlockAtNoCheck(Position position, Block block) {
+        blocks[position.getX()][position.getY()] = block;
+        block.move(position);
+    }
+
+    private boolean hasBlockAtNoCheck(Position position) {
+        return getBlockAtNoCheck(position) != null;
+    }
+
+    private void removeBlockAtNoCheck(Position position) {
+        blocks[position.getX()][position.getY()] = null;
     }
 }
